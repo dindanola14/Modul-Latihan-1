065e0b0f2eb5705bac8de432d7e158fc850710c4
Dinda Nola Dimelda <dinda_nola_29rpl@student.smktelkom-mlg.sch.id>
2021-01-14 20:05:56 +0700

    first commit

---
+++ PenerapanFor.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class PenerapanFor {
+   public static void main(String[] args){
+       for(int a=1; a<=1; a++){
+           for(int i=1; i<=4; i++){
+               System.out.print("@");
+           }
+           System.out.println();
+       }
+       for(int j=1; j<=2; j++){
+           for(int k=1; k<2; k++){
+               System.out.print("@");
+           }
+           for(int l=1; l<=2; l++){
+               System.out.print(" ");
+           }
+           for(int m=1; m<2; m++){
+               System.out.print("@");
+           }
+           System.out.println();
+       }
+       for(int b=1; b<=1; b++){
+           for(int i=1; i<=4; i++){
+               System.out.print("@");
+           }
+           System.out.println();
+       }
+   } 
+}
